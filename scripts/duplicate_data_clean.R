739449192efa4c1bdf7b27cd7d0eb2394f192654
Yujun Tong <150399434+YujunTong@users.noreply.github.com>
2025-11-14 22:23:59 +0000

    Add files via upload

---
+++ scripts/duplicate_data_clean.R
@@ -0,0 +1,32 @@
+# ==========================================
+# Remove fully duplicated rows based on:
+# 1. pvalue must be identical
+# 2. all other columns must also be identical， except the analysis_id and filename
+# ==========================================
+
+library(dplyr)
+
+# --- read the CSV file ---
+df <- read.csv("~/Desktop/Group1/clean_data.csv", stringsAsFactors = FALSE)
+# Columns used for second-level duplicate checking
+other_cols <- c("gene_symbol","gene_accession_id","mouse_strain","mouse_life_stage", "parameter_id", "parameter_name","analysis_id")   # <-- modify as needed
+# Check columns exist
+missing <- setdiff(c("pvalue", other_cols), names(df))
+if (length(missing) > 0) stop("Missing columns: ", paste(missing, collapse=", "))
+# ---- Two-step duplicate removal ----
+df_clean <- df %>%
+  group_by(pvalue) %>%                              # Step 1: only compare rows with same pvalue
+  distinct(across(all_of(other_cols)),              # Step 2: inside same pvalue, remove duplicates on selected columns
+           .keep_all = TRUE) %>%
+  ungroup()
+
+# Diagnostics
+cat("Original rows:", nrow(df), "\n")
+cat("Rows after cleaning:", nrow(df_clean), "\n")
+cat("Rows removed:", nrow(df) - nrow(df_clean), "\n")
+
+# Save output
+write.csv(df_clean, "clean_data_duplicate.csv", row.names = FALSE)
+cat("Saved to cleaned_by_pvalue_and_columns.csv\n")
+
+
