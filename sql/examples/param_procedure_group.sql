54091d355480bad12fc3fe1ef6a878f83f3a9db4
sofia-urosa <hedgehog.dilemma@outlook.com>
2025-11-25 01:29:18 +0000

    Fixed query

--- sql/examples/param_procedure_group.sql
+++ sql/examples/param_procedure_group.sql
@@ -6,10 +6,9 @@ SELECT
     p.parameter_id,
     p.parameter_name,
     dp.procedure_name,
-    pg.group_name
+    pgm.group_name
 FROM parameters p
 LEFT JOIN parameter_procedure pp USING (parameter_id)
 LEFT JOIN dim_procedure dp USING (procedure_id)
 LEFT JOIN parameter_group_map pgm USING (parameter_id)
-LEFT JOIN parameter_group pg USING (group_id)
-ORDER BY pg.group_name, dp.procedure_name;
+ORDER BY pgm.group_name, dp.procedure_name;
